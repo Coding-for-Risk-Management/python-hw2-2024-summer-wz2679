a2d23c324ddbd5a0cd471f6c3aef31100e4433e0
wz2679 <wz2679@columbia.edu>
2024-06-25 18:59:36 -0400

    Update FizzBuzz_File.py

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -1,27 +1,15 @@
-import numpy as np
-
 def FizzBuzz(start, finish):
-    # Create the number array
-    numvec = np.arange(start, finish+1, dtype=object)
+    outlist = []
+    for num in range(start, finish + 1):
+        if num % 3 == 0 and num % 5 == 0:
+            outlist.append("FizzBuzz")
+        elif num % 3 == 0:
+            outlist.append("Fizz")
+        elif num % 5 == 0:
+            outlist.append("Buzz")
+        else:
+            outlist.append(str(num))  # Convert number to string and append
     
-    # Initialize the array for results
-    objvec = np.array(numvec, dtype=object)
-    
-    # Vectorized FizzBuzz
-    fizz_mask = (numvec % 3 == 0)
-    buzz_mask = (numvec % 5 == 0)
-    fizzbuzz_mask = fizz_mask & buzz_mask
-    
-    objvec[fizzbuzz_mask] = "FizzBuzz"
-    objvec[fizz_mask & ~fizzbuzz_mask] = "Fizz"
-    objvec[buzz_mask & ~fizzbuzz_mask] = "Buzz"
-    
-    # Convert the result to a list by appending elements
-    myEmptyList = []
-    for elem in objvec:
-        myEmptyList.append(elem)
-    
-    return myEmptyList
-
+    return outlist
 
 
